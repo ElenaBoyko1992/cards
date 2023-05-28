a4307c1935689db51f5ee17631fda8981db02331
ElenaBoyko1992 <Bionka@inbox.ru>
2023-05-28 09:44:34 +0300

    28.05.2023

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -2,7 +2,7 @@ import { useAppDispatch } from "app/hooks";
 import { authThunks } from "features/auth/auth.slice";
 import s from "features/auth/login/Login.module.css";
 import { Button, Checkbox, FormControlLabel, Grid, Paper, TextField } from "@mui/material";
-import { Field, Form, FormikHelpers, useFormik } from "formik";
+import { Field, Form, FormikHelpers, FormikProvider, useFormik } from "formik";
 
 export const Login = () => {
   const dispatch = useAppDispatch();
@@ -44,15 +44,17 @@ export const Login = () => {
       <Grid item xs={4}>
         <Paper elevation={3}>
           <h1>Sign in</h1>
-          <Form>
-            <Field type="email" name="email" placeholder="Email" />
-            <Field type="password" name="password" placeholder="Password" />
-            <label>
-              <Field type="checkbox" name="rememberMe" />
-              {`Remember me`}
-            </label>
-            <button type="submit">Submit</button>
-          </Form>
+          <FormikProvider value={formik}>
+            <form action="" onSubmit={formik.handleSubmit}>
+              <Field type="email" name="email" placeholder="Email" />
+              <Field type="password" name="password" placeholder="Password" />
+              <label>
+                <Field type="checkbox" name="rememberMe" />
+                {`Remember me`}
+              </label>
+              <button type="submit">Submit</button>
+            </form>
+          </FormikProvider>
         </Paper>
       </Grid>
     </Grid>
