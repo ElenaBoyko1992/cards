029f61bff48b3bb41a470f771b64cf1933e00a3b
ElenaBoyko1992 <Bionka@inbox.ru>
2023-06-28 00:39:44 +0300

    28.06.2023

--- src/features/packs/BasicTable.tsx
+++ src/features/packs/BasicTable.tsx
@@ -8,10 +8,12 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useAppDispatch, useAppSelector } from "common/hooks";
 import { packsThunks } from "features/packs/packs.slice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { visuallyHidden } from "@mui/utils";
+import { TableSortLabel } from "@mui/material";
 
-function createData(name: string, cardsCount: number, updated: string, created: string, actions: string) {
-  return { name, cardsCount, updated, created, actions };
+function createData(name: string, cardsCount: number, updated: string, created: string, actions: string, id: string) {
+  return { name, cardsCount, updated, created, actions, id };
 }
 
 // const rows = [
@@ -26,8 +28,19 @@ export default function BasicTable() {
   const dispatch = useAppDispatch();
   const packs = useAppSelector((state) => state.packs.packsItems);
   const rows = packs.map((pack) => {
-    return createData(pack.name, pack.cardsCount, pack.updated, pack.created, "заглушка");
+    return createData(pack.name, pack.cardsCount, pack.updated, pack.created, "заглушка", pack._id);
   });
+
+  const [orderBy, setOrderBy] = useState<Order>("asc"); //1 - ask, 0 - desk
+  const sortNumber = orderBy === "asc" ? "1" : "0";
+
+  const [nameSortIsActive, setNameSortIsActive] = useState(false);
+
+  const sortByNameHandler = async () => {
+    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}updated` }));
+    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+    setNameSortIsActive(true);
+  };
 
   useEffect(() => {
     dispatch(packsThunks.getPacks({}));
@@ -38,7 +51,17 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
+            <TableCell>
+              Name
+              <TableSortLabel active={nameSortIsActive} direction={orderBy} onClick={sortByNameHandler}>
+                {/*{headCell.label}*/}
+                {/*{orderBy === headCell.id ? (*/}
+                {/*  <Box component="span" sx={visuallyHidden}>*/}
+                {/*    {order === "desc" ? "sorted descending" : "sorted ascending"}*/}
+                {/*  </Box>*/}
+                {/*) : null}*/}
+              </TableSortLabel>
+            </TableCell>
             <TableCell>Cards</TableCell>
             <TableCell>Last Updated</TableCell>
             <TableCell>Created by</TableCell>
@@ -47,7 +70,7 @@ export default function BasicTable() {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
+            <TableRow key={row.id}>
               <TableCell>{row.name}</TableCell>
               <TableCell>{row.cardsCount}</TableCell>
               <TableCell>{row.updated}</TableCell>
@@ -60,3 +83,6 @@ export default function BasicTable() {
     </TableContainer>
   );
 }
+
+//types
+type Order = "asc" | "desc";
