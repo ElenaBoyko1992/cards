93620fe0ad98c1ffd2fb375805f286cab7ced333
ElenaBoyko1992 <Bionka@inbox.ru>
2023-06-28 19:59:08 +0300

    28.06.2023

--- src/features/packs/BasicTable.tsx
+++ src/features/packs/BasicTable.tsx
@@ -8,9 +8,9 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useAppDispatch, useAppSelector } from "common/hooks";
 import { packsThunks } from "features/packs/packs.slice";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { visuallyHidden } from "@mui/utils";
-import { TableSortLabel } from "@mui/material";
+import { TablePagination, TableSortLabel } from "@mui/material";
 
 function createData(name: string, cardsCount: number, updated: string, created: string, actions: string, id: string) {
   return { name, cardsCount, updated, created, actions, id };
@@ -25,62 +25,137 @@ function createData(name: string, cardsCount: number, updated: string, created: 
 // ];
 
 export default function BasicTable() {
+  console.log("перерисовка");
   const dispatch = useAppDispatch();
   const packs = useAppSelector((state) => state.packs.packsItems);
+  const cardPacksTotalCount = useAppSelector((state) => state.packs.cardPacksTotalCount);
   const rows = packs.map((pack) => {
     return createData(pack.name, pack.cardsCount, pack.updated, pack.created, "заглушка", pack._id);
   });
 
   const [orderBy, setOrderBy] = useState<Order>("asc"); //1 - ask, 0 - desk
-  const sortNumber = orderBy === "asc" ? "1" : "0";
+  const sortNumber = orderBy === "asc" ? "0" : "1";
 
   const [nameSortIsActive, setNameSortIsActive] = useState(false);
+  const [cardsCountSortIsActive, setCardsCountSortIsActive] = useState(false);
+  const [lastUpdatedSortIsActive, setLastUpdatedSortIsActive] = useState(false);
+  const [createdSortIsActive, setCreatedSortIsActive] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = React.useState(0);
+
+  const tablePaginationCount = useMemo(() => {
+    return Math.ceil(cardPacksTotalCount / rowsPerPage);
+  }, [cardPacksTotalCount, rowsPerPage]);
 
   const sortByNameHandler = async () => {
-    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}updated` }));
-    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+    setCardsCountSortIsActive(false);
+    setLastUpdatedSortIsActive(false);
+    setCreatedSortIsActive(false);
     setNameSortIsActive(true);
+    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}name`, pageCount: rowsPerPage, page }));
+    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+  };
+
+  const sortByCardsCountHandler = async () => {
+    setNameSortIsActive(false);
+    setLastUpdatedSortIsActive(false);
+    setCreatedSortIsActive(false);
+    setCardsCountSortIsActive(true);
+    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}cardsCount`, pageCount: rowsPerPage, page }));
+    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+  };
+
+  const sortByLastUpdatedHandler = async () => {
+    setNameSortIsActive(false);
+    setCardsCountSortIsActive(false);
+    setCreatedSortIsActive(false);
+    setLastUpdatedSortIsActive(true);
+    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}updated`, pageCount: rowsPerPage, page }));
+    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+  };
+
+  const sortByCreatedHandler = async () => {
+    setNameSortIsActive(false);
+    setCardsCountSortIsActive(false);
+    setLastUpdatedSortIsActive(false);
+    setCreatedSortIsActive(true);
+    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}created`, pageCount: rowsPerPage, page }));
+    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   useEffect(() => {
-    dispatch(packsThunks.getPacks({}));
-  }, []);
+    dispatch(packsThunks.getPacks({ pageCount: rowsPerPage, page }));
+  }, [rowsPerPage]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              Name
-              <TableSortLabel active={nameSortIsActive} direction={orderBy} onClick={sortByNameHandler}>
-                {/*{headCell.label}*/}
-                {/*{orderBy === headCell.id ? (*/}
-                {/*  <Box component="span" sx={visuallyHidden}>*/}
-                {/*    {order === "desc" ? "sorted descending" : "sorted ascending"}*/}
-                {/*  </Box>*/}
-                {/*) : null}*/}
-              </TableSortLabel>
-            </TableCell>
-            <TableCell>Cards</TableCell>
-            <TableCell>Last Updated</TableCell>
-            <TableCell>Created by</TableCell>
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.cardsCount}</TableCell>
-              <TableCell>{row.updated}</TableCell>
-              <TableCell>{row.created}</TableCell>
-              <TableCell>{row.actions}</TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Name
+                <TableSortLabel
+                  active={nameSortIsActive}
+                  direction={orderBy}
+                  onClick={sortByNameHandler}
+                ></TableSortLabel>
+              </TableCell>
+              <TableCell>
+                Cards
+                <TableSortLabel
+                  active={cardsCountSortIsActive}
+                  direction={orderBy}
+                  onClick={sortByCardsCountHandler}
+                ></TableSortLabel>
+              </TableCell>
+              <TableCell>
+                Last Updated
+                <TableSortLabel
+                  active={lastUpdatedSortIsActive}
+                  direction={orderBy}
+                  onClick={sortByLastUpdatedHandler}
+                ></TableSortLabel>
+              </TableCell>
+              <TableCell>
+                Created by
+                <TableSortLabel
+                  active={createdSortIsActive}
+                  direction={orderBy}
+                  onClick={sortByCreatedHandler}
+                ></TableSortLabel>
+              </TableCell>
+              <TableCell>Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.cardsCount}</TableCell>
+                <TableCell>{row.updated}</TableCell>
+                <TableCell>{row.created}</TableCell>
+                <TableCell>{row.actions}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={tablePaginationCount}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={() => {}}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
   );
 }
 
--- src/features/packs/packs.slice.ts
+++ src/features/packs/packs.slice.ts
@@ -3,23 +3,28 @@ import { createAppAsyncThunk, thunkTryCatch } from "common/utils";
 import { packsApi, PacksType, PackType } from "features/packs/packs.api";
 import { ProfileType } from "features/auth/auth.api";
 
-const getPacks = createAppAsyncThunk<{ packsItems: PacksType }, any>("packs/getPacks", async (arg, thunkAPI) => {
-  return thunkTryCatch(thunkAPI, async () => {
-    const res = await packsApi.getPacks(arg);
+const getPacks = createAppAsyncThunk<{ packsItems: PacksType; cardPacksTotalCount: number }, any>(
+  "packs/getPacks",
+  async (arg, thunkAPI) => {
+    return thunkTryCatch(thunkAPI, async () => {
+      const res = await packsApi.getPacks(arg);
 
-    return { packsItems: res.data.cardPacks };
-  });
-});
+      return { packsItems: res.data.cardPacks, cardPacksTotalCount: res.data.cardPacksTotalCount };
+    });
+  }
+);
 
 const slice = createSlice({
   name: "packs",
   initialState: {
     packsItems: [] as PacksType,
+    cardPacksTotalCount: 0,
   },
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getPacks.fulfilled, (state, action) => {
       state.packsItems = action.payload.packsItems;
+      state.cardPacksTotalCount = action.payload.cardPacksTotalCount;
     });
   },
 });
