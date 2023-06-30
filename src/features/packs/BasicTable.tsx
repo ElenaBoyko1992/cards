82c2d7dec3c24aab6951f7678d5d57e40d47f208
ElenaBoyko1992 <Bionka@inbox.ru>
2023-06-30 20:19:02 +0300

    30.06.2023

--- src/features/packs/BasicTable.tsx
+++ src/features/packs/BasicTable.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect, useMemo, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,9 +9,22 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useAppDispatch, useAppSelector } from "common/hooks";
 import { packsThunks } from "features/packs/packs.slice";
-import { useEffect, useMemo, useState } from "react";
-import { visuallyHidden } from "@mui/utils";
-import { TablePagination, TableSortLabel } from "@mui/material";
+import {
+  Input,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Pagination,
+  Select,
+  SelectChangeEvent,
+  TableSortLabel,
+  TextField,
+} from "@mui/material";
+import s from "./Packs.module.css";
+import { AccountCircle } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useDebounce } from "common/hooks/useDebounce";
 
 function createData(name: string, cardsCount: number, updated: string, created: string, actions: string, id: string) {
   return { name, cardsCount, updated, created, actions, id };
@@ -34,7 +48,7 @@ export default function BasicTable() {
   });
 
   const [orderBy, setOrderBy] = useState<Order>("asc"); //1 - ask, 0 - desk
-  const sortNumber = orderBy === "asc" ? "0" : "1";
+  const sortNumber = orderBy === "asc" ? "1" : "0";
 
   const [nameSortIsActive, setNameSortIsActive] = useState(false);
   const [cardsCountSortIsActive, setCardsCountSortIsActive] = useState(false);
@@ -42,6 +56,9 @@ export default function BasicTable() {
   const [createdSortIsActive, setCreatedSortIsActive] = useState(false);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [page, setPage] = React.useState(0);
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const debouncedValue = useDebounce<string>(searchValue, 500);
 
   const tablePaginationCount = useMemo(() => {
     return Math.ceil(cardPacksTotalCount / rowsPerPage);
@@ -52,7 +69,7 @@ export default function BasicTable() {
     setLastUpdatedSortIsActive(false);
     setCreatedSortIsActive(false);
     setNameSortIsActive(true);
-    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}name`, pageCount: rowsPerPage, page }));
+    // await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}name`, pageCount: rowsPerPage, page }));
     setOrderBy(orderBy === "asc" ? "desc" : "asc");
   };
 
@@ -61,8 +78,9 @@ export default function BasicTable() {
     setLastUpdatedSortIsActive(false);
     setCreatedSortIsActive(false);
     setCardsCountSortIsActive(true);
-    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}cardsCount`, pageCount: rowsPerPage, page }));
     setOrderBy(orderBy === "asc" ? "desc" : "asc");
+
+    //await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}cardsCount`, pageCount: rowsPerPage, page }));
   };
 
   const sortByLastUpdatedHandler = async () => {
@@ -70,7 +88,7 @@ export default function BasicTable() {
     setCardsCountSortIsActive(false);
     setCreatedSortIsActive(false);
     setLastUpdatedSortIsActive(true);
-    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}updated`, pageCount: rowsPerPage, page }));
+    // await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}updated`, pageCount: rowsPerPage, page }));
     setOrderBy(orderBy === "asc" ? "desc" : "asc");
   };
 
@@ -79,21 +97,74 @@ export default function BasicTable() {
     setCardsCountSortIsActive(false);
     setLastUpdatedSortIsActive(false);
     setCreatedSortIsActive(true);
-    await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}created`, pageCount: rowsPerPage, page }));
+    // await dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}created`, pageCount: rowsPerPage, page }));
     setOrderBy(orderBy === "asc" ? "desc" : "asc");
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
+  const handleChangeRowsPerPage = (event: SelectChangeEvent) => {
+    setRowsPerPage(parseInt(event.target.value, 10) as number);
   };
 
+  const paginationChangedHandler = async (event: React.ChangeEvent<unknown>, page: number) => {
+    setPage(page);
+  };
+
+  const searchHandler = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  // useEffect(() => {
+  //   dispatch(packsThunks.getPacks({ pageCount: rowsPerPage, page, packName: searchValue }));
+  // }, [debouncedValue]);
+
   useEffect(() => {
-    dispatch(packsThunks.getPacks({ pageCount: rowsPerPage, page }));
-  }, [rowsPerPage]);
+    // dispatch(packsThunks.getPacks({ pageCount: rowsPerPage, page }));
+    if (searchValue) {
+      dispatch(packsThunks.getPacks({ pageCount: rowsPerPage, page, packName: searchValue }));
+    } else if (nameSortIsActive) {
+      dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}name`, pageCount: rowsPerPage, page }));
+    } else if (cardsCountSortIsActive) {
+      dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}cardsCount`, pageCount: rowsPerPage, page }));
+    } else if (lastUpdatedSortIsActive) {
+      dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}updated`, pageCount: rowsPerPage, page }));
+    } else if (createdSortIsActive) {
+      dispatch(packsThunks.getPacks({ sortPacks: `${sortNumber}created`, pageCount: rowsPerPage, page }));
+    } else {
+      dispatch(packsThunks.getPacks({ pageCount: rowsPerPage, page }));
+    }
+    // cardsCountSortIsActive && sortByCardsCountHandler();
+    // lastUpdatedSortIsActive && sortByLastUpdatedHandler();
+    // createdSortIsActive && sortByCreatedHandler();
+  }, [
+    page,
+    rowsPerPage,
+    orderBy,
+    nameSortIsActive,
+    cardsCountSortIsActive,
+    lastUpdatedSortIsActive,
+    createdSortIsActive,
+    debouncedValue,
+  ]);
 
   return (
     <>
+      <div className={s.tableFilters}>
+        <InputLabel shrink htmlFor="search-input" className={s.inputLabelText}>
+          Search
+        </InputLabel>
+        <OutlinedInput
+          id="search-input"
+          onChange={searchHandler}
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+          placeholder="Provide your text"
+          size="small"
+        />
+        {/*<TextField placeholder="Outlined" variant="outlined" id="search-input" />*/}
+      </div>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -146,15 +217,38 @@ export default function BasicTable() {
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={tablePaginationCount}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={() => {}}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+      <div className={s.paginationBlock}>
+        <Pagination
+          count={tablePaginationCount}
+          color="primary"
+          shape="rounded"
+          size="small"
+          onChange={paginationChangedHandler}
+          className={s.tablePagination}
+        />
+        {/*<TablePagination*/}
+        {/*  rowsPerPageOptions={[5, 10, 25]}*/}
+        {/*  component="div"*/}
+        {/*  count={tablePaginationCount}*/}
+        {/*  rowsPerPage={rowsPerPage}*/}
+        {/*  page={page}*/}
+        {/*  onPageChange={() => {}}*/}
+        {/*  onRowsPerPageChange={handleChangeRowsPerPage}*/}
+        {/*  labelRowsPerPage={"Show"}*/}
+        {/*/>*/}
+        <span>Show</span>
+        <Select
+          value={rowsPerPage.toString()}
+          onChange={handleChangeRowsPerPage}
+          className={s.tableSelectRowPerPage}
+          size="small"
+        >
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={25}>25</MenuItem>
+        </Select>
+        <span>Cards per page</span>
+      </div>
     </>
   );
 }
